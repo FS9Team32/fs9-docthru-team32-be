27bcbe52d05b9a64058c80ac4c7786d2f5ec71fd
bean-baek <sbaek4908@naver.com>
2025-12-04 10:25:40 +0900

    chore : init new schema

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,68 @@
 import express from 'express';
-// import { authRepo } from '../repository/auth.repo.js';
+import { expressjwt } from "express-jwt";
+import authService from '../services/auth.service.js';
+import auth from "../middlewares/auth.js";
 
-const router = express.Router();
+const authRouter = express.Router();
 
-export default router;
+const verifyAccessToken = expressjwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ['HS256']
+});
+
+
+authRouter.post("/signup", async (req, res, next) => {
+    try {
+        const { email, nickname, password } = req.body;
+        if (!email || !nickname || !password) {
+        const error = new Error("email, nickname, password 가 모두 필요합니다.");
+        error.code = 400;
+        throw error;
+        }
+        const user = await authService.createUser({ email, nickname, password });
+        res.status(201).json(user);
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+authRouter.post ("/login", async(req, res, next) => {
+    const {email, password} = req.body;
+    try {
+        if (!email || !password) {
+            const error = new Error ("Requires both email and Password");
+            error.code = 400;
+            throw error;
+        }
+        const user = await authService.getUser(email,password);
+        req.session.userId = user.id;
+        res.json(user);
+    } catch (error) {
+        next(error);
+    }
+})
+
+authRouter.post(
+    "/token/refresh",
+    auth.verifyRefreshToken,
+    async (req, res, next) => {
+        try {
+        const refreshToken = req.cookies.refreshToken;
+        const { userId } = req.auth;
+        const { newAccessToken, newRefreshToken } =
+            await authService.refreshToken(userId, refreshToken);
+        res.cookie("refreshToken", newRefreshToken, {
+            httpOnly: true,
+            sameSite: "none",
+            secure: true,
+            path: "/token/refresh",
+        });
+        return res.json({ accessToken: newAccessToken });
+        } catch (error) {
+        return next(error);
+        }
+    },
+);
+
+export default authRouter;
---
+++ src/services/auth.service.js
@@ -0,0 +1,95 @@
+import bcrypt from "bcrypt";
+import authRepo from "../repository/auth.repo.js";
+import jwt from "jsonwebtoken"
+
+async function createUser(user) {
+    try{
+        const existedUser = await authRepo.findByEmail(user.email);
+        if (existedUser) {
+            const error = new Error("User already exists");
+            error.code = 409;
+            error.data = {email: user.email};
+            throw error;
+        }
+    const hashedPassword = await hashPassword(user.password);
+    const createdUser = await authRepo.save({
+        ...user,
+        password: hashedPassword,
+        });    
+        return filterSensitiveUserData(createdUser);
+    } catch (error){
+        if (error.code === 409) throw error;
+        const customError = new Error ("DB Error Accured");
+        customError.code = 500;
+        throw customError;
+    }
+}
+    function hashPassword(password) {
+    return bcrypt.hash(password, 10);
+    }
+
+    function filterSensitiveUserData(user) {
+    const { password, ...rest } = user;
+    return rest;
+}
+async function getUser(email, password) {
+    try {
+        const user = await authRepo.findByEmail(email);
+    if (!user){
+            const error = new Error("Email does not exists");
+            error.code = 401;
+            throw error;
+        }
+        await verifyPassword(password, user.password);
+        return filterSensitiveUserData(user);
+    } catch (error) {
+        if (error.code === 401) throw error;
+        const customError = new Error("DB Error Accured");
+        customError.code = 500;
+        throw customError;
+    }
+}
+
+    async function verifyPassword(inputPassword, password) {
+    const isMatch = await bcrypt.compare(inputPassword, password);
+    if (!isMatch) {
+        const error = new Error("비밀번호가 일치하지 않습니다.");
+        error.code = 401;
+        throw error;
+    }
+}
+
+function createToken(user, type) {
+    const payload = { userId: user.id };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: type === "refresh" ? "2w" : "1h",
+    });
+    return token;
+}
+
+async function updateUser(id, data) {
+    const updatedUser = await authRepo.update(id, data);
+    return filterSensitiveUserData(updatedUser);
+}
+
+async function refreshToken(userId, refreshToken) {
+    const user = await authRepo.findById(userId);
+    if (!user || user.refreshToken !== refreshToken) {
+        const error = new Error("Unauthorized");
+        error.code = 401;
+        throw error;
+    }
+
+    const newAccessToken = createToken(user);
+    const newRefreshToken = createToken(user, "refresh");
+    return { newAccessToken, newRefreshToken };
+}
+
+
+export default{
+    createUser,
+    getUser,
+    updateUser,
+    createToken,
+    refreshToken,
+};
