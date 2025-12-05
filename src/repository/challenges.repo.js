7d099ee5f4433e9c3007e09e0b9219abfc53f889
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-12-05 15:45:27 +0900

    feat: create challenge.repo for work sevices logic

--- src/repository/challenges.repo.js
+++ src/repository/challenges.repo.js
@@ -1,3 +1,21 @@
-// import { prisma } from '../db/prisma.js';
+import { prisma } from '../db/prisma.js';
 
-export const challengesRepo = {};
+async function findChallengeById(challengeId, tx) {
+  const db = tx || prisma;
+  return db.challenge.findUnique({
+    where: { id: Number(challengeId) },
+  });
+}
+
+async function updateChallengeStatus(challengeId, status, tx) {
+  const db = tx || prisma;
+  return db.challenge.update({
+    where: { id: Number(challengeId) },
+    data: { status },
+  });
+}
+
+export const challengesRepo = {
+  findChallengeById,
+  updateChallengeStatus,
+};
