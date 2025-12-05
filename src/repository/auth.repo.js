d303d8c0b02cf44e22b359ba1a1961cd1fcda639
Lim Gyungmin <65752686+play-ancora-gyungmin@users.noreply.github.com>
2025-12-05 10:34:56 +0900

    Merge pull request #5 from FS9Team32/feat/auth
    수정 요청 사항 /auth 와 .env 상세 수정

--- src/repository/auth.repo.js
+++ src/repository/auth.repo.js
@@ -1,3 +1,52 @@
-// import { prisma } from '../db/prisma.js';
+import { prisma } from '../db/prisma.js';
 
-export const authRepo = {};
+async function findById(id) {
+  return prisma.user.findUnique({
+    where: {
+      id,
+    },
+  });
+}
+
+async function findByEmail(email) {
+  return await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+}
+
+async function save(user) {
+  return prisma.user.create({
+    data: {
+      email: user.email,
+      nickname: user.nickname,
+      password: user.password,
+    },
+  });
+}
+
+async function update(id, data) {
+  return prisma.user.update({
+    where: {
+      id,
+    },
+    data: data,
+  });
+}
+
+async function createOrUpdate(provider, providerId, email, nickname) {
+  return prisma.user.upsert({
+    where: { provider, providerId },
+    update: { email, nickname },
+    create: { provider, providerId, email, nickname },
+  });
+}
+
+export default {
+  findById,
+  findByEmail,
+  save,
+  update,
+  createOrUpdate,
+};
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,70 @@
 import express from 'express';
-// import { authRepo } from '../repository/auth.repo.js';
+import authService from '../services/auth.service.js';
+import auth from '../middlewares/auth.js';
 
-const router = express.Router();
+const authRouter = express.Router();
 
-export default router;
+authRouter.post('/signup', async (req, res, next) => {
+  try {
+    const { email, nickname, password } = req.body;
+    if (!email || !nickname || !password) {
+      const error = new Error('All Input Is Required');
+      error.code = 400;
+      throw error;
+    }
+    const user = await authService.createUser({ email, nickname, password });
+    res.status(201).json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+authRouter.post('/login', async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      const error = new Error('Email And Password Is Required');
+      error.code = 400;
+      throw error;
+    }
+    const user = await authService.getUser(email, password);
+    const accessToken = authService.createToken(user);
+    const refreshToken = authService.createToken(user, 'refresh');
+
+    await authService.updateUser(user.id, { refreshToken });
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      sameSite: 'lax',
+      secure: false,
+    });
+    res.json({ ...user, accessToken });
+  } catch (error) {
+    next(error);
+  }
+});
+
+authRouter.post(
+  '/token/refresh',
+  auth.verifyRefreshToken,
+  async (req, res, next) => {
+    try {
+      const { userId } = req.auth;
+      const refreshToken = req.cookies.refreshToken;
+      const { newAccessToken, newRefreshToken } =
+        await authService.refreshToken(userId, refreshToken);
+
+      res.cookie('refreshToken', newRefreshToken, {
+        httpOnly: true,
+        sameSite: 'lax',
+        secure: false,
+        path: '/token/refresh',
+      });
+      return res.json({ accessToken: newAccessToken });
+    } catch (error) {
+      return next(error);
+    }
+  },
+);
+
+export default authRouter;
