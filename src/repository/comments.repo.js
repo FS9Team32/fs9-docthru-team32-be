52b9d24d54f69bcb7957ba4896ff48cb828da5d7
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-12-08 13:34:40 +0900

    feat: create comment repository methods

--- src/repository/comments.repo.js
+++ src/repository/comments.repo.js
@@ -1,3 +1,51 @@
-// import { prisma } from '../db/prisma.js';
+import { prisma } from '../db/prisma.js';
 
-export const commentsRepo = {};
+async function createComment({ workId, userId, content }) {
+  return prisma.comment.create({
+    data: {
+      workId: Number(workId),
+      authorId: Number(userId),
+      content,
+    },
+  });
+}
+
+async function getCommentsListByWorkId({
+  workId,
+  limit = 10,
+  cursorId = null,
+}) {
+  const queryOptions = {
+    where: { workId: Number(workId) },
+    take: limit,
+    orderBy: { createdAt: 'desc' },
+  };
+  if (cursorId) {
+    queryOptions.cursor = { id: cursorId };
+    queryOptions.skip = 1;
+  }
+  const commentsList = await prisma.comment.findMany(queryOptions);
+  let nextCursor = null;
+  if (commentsList.length === limit) {
+    nextCursor = commentsList[commentsList.length - 1].id;
+  }
+  return {
+    list: commentsList,
+    nextCursor,
+  };
+}
+
+async function updateComment({ commentId, data }) {
+  return prisma.comment.update({ where: { id: Number(commentId) }, data });
+}
+
+async function deleteComment(commentId) {
+  return prisma.comment.delete({ where: { id: Number(commentId) } });
+}
+
+export const commentsRepo = {
+  createComment,
+  getCommentsListByWorkId,
+  updateComment,
+  deleteComment,
+};
