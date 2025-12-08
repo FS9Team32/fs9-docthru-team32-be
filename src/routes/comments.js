187ddb544bfb8227f7b51820e859fcd76544d1cc
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-12-08 17:05:15 +0900

    feat: comments routes methodes

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -1,6 +1,43 @@
 import express from 'express';
-// import { commentsRepo } from '../repository/comments.repo.js';
+import commentsServices from '../services/comments.services';
+import auth from '../middlewares/auth';
 
 const router = express.Router();
+
+router.patch('/:commentId', auth.verifyAccessToken, async (req, res, next) => {
+  try {
+    const { commentId } = req.params;
+    const { userId } = req.auth;
+    const { content } = req.body;
+    const updatedComment = await commentsServices.updateComment({
+      commentId: Number(commentId),
+      userId,
+      content,
+    });
+    res.status(200).json({
+      success: true,
+      ...updatedComment,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:commentId', auth.verifyAccessToken, async (req, res, next) => {
+  try {
+    const { commentId } = req.params;
+    const { userId } = req.auth;
+    const deletedComment = await commentsServices.deleteComment({
+      commentId: Number(commentId),
+      userId,
+    });
+    res.status(200).json({
+      success: true,
+      ...deletedComment,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 
 export default router;
--- src/routes/works.comments.js
+++ src/routes/works.comments.js
@@ -1,6 +1,48 @@
 import express from 'express';
-// import { commentsRepo } from '../repository/comments.repo.js';
+import commentsServices from '../services/comments.services';
+import auth from '../middlewares/auth';
 
 const router = express.Router({ mergeParams: true });
+
+router.post('/', auth.verifyAccessToken, async (req, res, next) => {
+  try {
+    const { workId } = req.params;
+    const { userId } = req.auth;
+    const { content } = req.body;
+    const newComment = await commentsServices.createComment({
+      workId: Number(workId),
+      userId,
+      content,
+    });
+    res.status(201).json({
+      success: true,
+      ...newComment,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/', async (req, res, next) => {
+  try {
+    const { workId } = req.params;
+    const { limit, cursor } = req.query;
+    const getListParams = {
+      workId,
+      limit,
+    };
+    if (cursor) {
+      getListParams.cursorId = cursor;
+    }
+    const commentList =
+      await commentsServices.getWorkCommentsList(getListParams);
+    res.status(200).json({
+      success: true,
+      ...commentList,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 
 export default router;
