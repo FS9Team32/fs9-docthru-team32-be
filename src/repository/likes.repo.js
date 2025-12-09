9e98d4ae179fdb07d8dcba817e9727470aa81e21
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-12-09 14:11:28 +0900

    feat: likes repository

--- src/repository/likes.repo.js
+++ src/repository/likes.repo.js
@@ -1,3 +1,37 @@
-// import { prisma } from '../db/prisma.js';
+import { prisma } from '../db/prisma.js';
 
-export const likesRepo = {};
+async function findLike({ workId, userId }, tx) {
+  const db = tx || prisma;
+  return db.like.findUnique({
+    where: {
+      workId_userId: {
+        workId: Number(workId),
+        userId: Number(userId),
+      },
+    },
+  });
+}
+
+async function createLike({ workId, userId }, tx) {
+  const db = tx || prisma;
+  return db.like.create({
+    data: {
+      workId: Number(workId),
+      userId: Number(userId),
+    },
+  });
+}
+
+async function deleteLike({ workId, userId }, tx) {
+  const db = tx || prisma;
+  return db.like.delete({
+    where: {
+      workId_userId: {
+        workId: Number(workId),
+        userId: Number(userId),
+      },
+    },
+  });
+}
+
+export const likesRepo = { findLike, createLike, deleteLike };
