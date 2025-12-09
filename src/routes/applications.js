ab7fd0a1f7eb2e1342158f1e800a9ba4375106a4
Lim Gyungmin <65752686+play-ancora-gyungmin@users.noreply.github.com>
2025-12-09 16:48:20 +0900

    Revert "payload 추가, try catch 삭제, 정확한 에러 코드로 수정"

--- src/routes/applications.js
+++ src/routes/applications.js
@@ -1,5 +1,5 @@
 import express from 'express';
-// import { applicationRepo } from '../repository/challenges.repo.js';
+// import { challengesRepo } from '../repository/challenges.repo.js';
 
 const router = express.Router();
 
